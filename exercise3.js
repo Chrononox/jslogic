81d5ab6c7e1cd5626e6e357c28237c37890b36e5
Chrononox <tegbert@gmail.com>
2020-01-31 12:11:14 -0600

    logic mapped out/README update

--- exercise3.js
+++ exercise3.js
@@ -1,1 +1,20 @@
 // Question 3: Write a function that converts HEX to RGB. Then Make that function auto-dect the formats so that if you enter HEX color format it returns RGB and if you enter RGB color format it returns HEX.
+
+//input from user
+//display on page
+
+const converter = ((input) => {
+    //decide if hex or rgb
+    //swap
+    //display
+})
+
+//input hex return rgb
+const hexConvert = ((hex) => {
+//parseInt("ff", 16) ~ convert hex to num
+})
+
+//input rgb return hex
+const rgbConvert  =((rgb) => {
+//toString(16)~ converts num to hex string
+})
