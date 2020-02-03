d3f3f59669718ced3778cd809f27b17613ca3b00
Travis <tegbert@gmail.com>
2020-02-02 18:19:48 -0600

    started exercise 2

--- exercise2.js
+++ exercise2.js
@@ -1,15 +1,13 @@
-// Question 2: Write a javascript function that takes an array of numbers and a target number. The function should find two different numbers in the array that, when added together, give the target number. For example: answer([1,2,3], 4)should return [1,3]
-console.log('------------exercise2.js----------------')
+let ex2clicked = document.getElementById("ex2click");
+let ex2Output = document.getElementById("ex2output");
+
 const findMath = ((arr, num) => {
-    //iterate over arr twice and check the math is equal to num
-    //return two numbers in own array(?) or retun 'no match found'
     let answer = '';
     let found = false;
     arr.map((item, index) => {
         for(i=0; i < arr.length; i++) {
             
             if ((item + arr[i]) === num && i !== index && !found){
-                //console.log(`+.+`);
                 answer = `${item} + ${arr[i]} = ${num}`
                 found = !found;
             }
@@ -18,7 +16,13 @@ const findMath = ((arr, num) => {
     return answer;
 })
 
-console.log(findMath([1,2,3,4,5],8))
+//get user input return answer
+ex2clicked.addEventListener("click", function() {
+    let userInput = document.getElementById("ex2input");
+    let userSum = document.getElementById("ex2sum");
 
-//display on page
-//get user input?
+    let array = userInput.value.split(',');
+    let newArray = findMath(array, userSum);
+
+    ex2Output.innerHTML = `[${newArray}]`;
+})
